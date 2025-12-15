a182c91f01697a2a2452fbedc128ba5d88773d3d
priyamraj201-art <priyam.raj.201@gmail.com>
2025-12-15 14:05:36 +0000

    Container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountID = 145253
+let accountEmail = "priyam@nhrbv.com" 
+var accountPassword ="hrgvaz"
+accountCity = "Ranchi"
+let accountState;
+
+// accountID = 2 // not allowed
+
+/*dot use var as it have some problems so use let */
+
+accountEmail = "uhf@gmail.com"
+accountPassword ="123"
+accountCity="PAtna"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+// number >> 2 to power 53
+// bigint
+// string => 1111
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol unique
+I
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
