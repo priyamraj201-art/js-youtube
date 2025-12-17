c50874552db2bb6f1a237a61d1f78c9101a07212
priyamraj201-art <priyam.raj.201@gmail.com>
2025-12-17 15:20:49 +0000

    config done

---
+++ 01_basics/03_conversionVar.js
@@ -0,0 +1,10 @@
+let score = "33abs"
+console.log(typeof score);
+
+let valueNumber = Number(score)   // Number(score) here Number converts datatype of score 
+console.log(typeof valueNumber);
+console.log( valueNumber);
+
+let loggedIn = 1
+let IsLoggedIn = Boolean(loggedIn)
+console.log(IsLoggedIn);
