848d83bf22a21d401eb8546565c070431425eb8f
priyamraj201-art <priyam.raj.201@gmail.com>
2026-01-05 16:11:13 +0000

    configr done

---
+++ 01_basics/04_copmarison.js
@@ -0,0 +1,5 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+//this is because comparison and equality operator works differently comparison <=,>,< converts mnull into a number soo (1) is false and (3) is true
